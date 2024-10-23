2c30b46342d1925339d19c64a7fb6deae25e30f1
elias <elj@hoermann-info.com>
2024-10-23 11:41:36 +0200

    Add project files.

---
+++ NotifyIconTest/Program.cs
@@ -0,0 +1,55 @@
+namespace NotifyIconTest
+{
+    internal static class Program
+    {
+
+        public static NotifyIcon TrayIcon { get; set; } = new();
+
+
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            LoadContextMenu();
+            Application.Run();
+        }
+
+        static void LoadContextMenu()
+        {
+            var contextMenu = new ContextMenuStrip();
+            contextMenu.Items.AddRange(new ToolStripMenuItem[]
+            {
+                new("Send warning", null, (sender, e) => SendWarning()),
+                new("Send error", null, (sender, e) => SendError()),
+                new("Send question", null, (sender, e) => SendQuestion()),
+            });
+
+            TrayIcon.ContextMenuStrip = contextMenu;
+            TrayIcon.Icon = SystemIcons.Information;
+            TrayIcon.Visible = true;
+        }
+
+        static void SendWarning()
+        {
+            TrayIcon.Icon = SystemIcons.Warning;
+            TrayIcon.BalloonTipText = "Warning";
+            TrayIcon.ShowBalloonTip(3);
+        }
+
+        static void SendError()
+        {
+            TrayIcon.Icon = SystemIcons.Error;
+            TrayIcon.BalloonTipText = "Error";
+            TrayIcon.ShowBalloonTip(3);
+        }
+
+        static void SendQuestion()
+        {
+            TrayIcon.Icon = SystemIcons.Question;
+            TrayIcon.BalloonTipText = "Question";
+            TrayIcon.ShowBalloonTip(3);
+        }
+    }
+}
